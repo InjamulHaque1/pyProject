9dacb085b5506900e92ae71ede8ce16fce9340a5
Hossian200e <hossain200e@gmail.com>
2023-08-10 00:09:53 +0600

    list

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ print("Hello,I am team member in python lab project and my name is Md Hossain")
 a = "Hello World!"
 print(a.upper())
 print(len(a))
+
+#Again change by Hossian
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
