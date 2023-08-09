c385546ca88afc4d50c1221e602c19883b61102f
Injamul Haque Navid <injamulhaque1223@gmail.com>
2023-08-10 00:20:04 +0600

    palindrome

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ print("Hello,I am team member in python lab project and my name is Md Hossain")
 a = "Hello World!"
 print(a.upper())
 print(len(a))
+
+def fun(word):
+    newWord = word[::-1]
+    if newWord == word:
+        print("Palindrome")
+    else:
+        print("Not palindrome")
+fun("racecar")
